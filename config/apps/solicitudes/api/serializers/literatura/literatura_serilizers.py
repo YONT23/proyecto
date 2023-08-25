509967b1566c0b2c1841b40eefd8932c97cf2010
Yonatha Mendoza <100289921+YONT23@users.noreply.github.com>
2023-08-24 21:35:32 -0500

    24/08/23
    backend

--- config/apps/solicitudes/api/serializers/literatura/literatura_serilizers.py
+++ config/apps/solicitudes/api/serializers/literatura/literatura_serilizers.py
@@ -1,33 +1,9 @@
 from rest_framework import serializers
 from ....models import *
-
-#class ArticuloSerializer(serializers.ModelSerializer):
-#    class Meta:
-#        model = Artículos
-#        fields = '__all__'
-#
-#class LibrosSerializer(serializers.ModelSerializer):
-#    class Meta:
-#        model = Libros
-#        fields = '__all__'
-#
-#class CapítuloslibrosSerializer(serializers.ModelSerializer):
-#    class Meta:
-#        model = Capítuloslibros
-#        fields = '__all__'
-#
-#class LiteraturaSerializer(serializers.ModelSerializer):
-#    articulo = ArticuloSerializer()
-#    libros = LibrosSerializer()
-#    capitulos = CapítuloslibrosSerializer()
-#
-#    class Meta:
-#        model = Literatura
-#        fields = '__all__'
         
 class ContenidoSolicitudSerializer(serializers.ModelSerializer):
-    #literact_citada = LiteraturaSerializer()
-
+    archivo_adjunto = serializers.FileField()  # Agregar esta línea para manejar archivos adjuntos
+    
     class Meta:
         model = ContenidoSolicitud
         fields = '__all__'
